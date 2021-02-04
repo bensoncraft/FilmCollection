767c70ba629922711b99d42fa12dd8a714a692a8
bensoncraft <71245174+bensoncraft@users.noreply.github.com>
2021-02-03 23:33:26 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovies(ApplicationResponse appResponse)
+        {
+            TempStorage.AddMovie(appResponse);
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Movies.Where(x => x.Title != "Independence Day")); //Code to exclude Independence Day from showing in the list on MovieList page
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmCollection.Models
+{
+    public class ApplicationResponse
+    {
+        [Required(ErrorMessage = "Please enter a Category")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Director")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Please select a rating")]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
